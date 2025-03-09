c358715ead8c394958c458a90ca4fcc5f5d9a028
Kazi Mohammad Kaif <130026107+kazikaif@users.noreply.github.com>
2025-03-09 12:44:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+document.getElementById("userForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+  const name = document.getElementById("name").value.trim();
+  const age = parseInt(document.getElementById("age").value, 10);
+
+  if (!name || isNaN(age)) {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age > 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000);
+  })
+    .then((message) => alert(message))
+    .catch((message) => alert(message));
+});
